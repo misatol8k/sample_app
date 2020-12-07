259b1869508fc5bff5d6193d30dc0cf22c5390da
misatol8k <misatolkurihara@gmail.com>
2020-12-08 00:22:17 +0900

    Complete advanced setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,11 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
+# require File.expand_path('../../config/environment', __FILE__)
+# チュートリアルはコメントアウトした行を使用
+
 require 'rails/test_help'
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
